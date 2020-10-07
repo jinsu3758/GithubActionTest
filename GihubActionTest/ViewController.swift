da12ff0bc61c6ee5f328b811a6ee195432d5c932
jinsu3758 <jinsu3758@naver.com>
2020-10-07 17:12:52 +0900

    addCount 추가

--- GihubActionTest/ViewController.swift
+++ GihubActionTest/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func addCount(_ sender: Any) {
+        let count = addCount(Int(countLabel.text ?? "0") ?? 0)
+        countLabel.text = count.description
+    }
+    
+    @IBAction func resetCount(_ sender: Any) {
+        countLabel.text = "0"
+    }
+    
+    func addCount(_ data: Int) -> Int {
+        return data + 1
+    }
+    
 }
 
--- GihubActionTestTests/GihubActionTestTests.swift
+++ GihubActionTestTests/GihubActionTestTests.swift
@@ -17,17 +17,15 @@ class GihubActionTestTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_addCount() {
+        let vc = ViewController()
+        
+        let result = vc.addCount(0)
+        let expectedResult = 1
+        
+        XCTAssertEqual(result, expectedResult, "카운트 실패")
+        
     }
 
 }
